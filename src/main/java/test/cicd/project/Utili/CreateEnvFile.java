a533f458f13143a299f11eb49cffb2172dd90741
jun <hujy11@gmail.com>
2018-12-13 21:59:52 +0800

    test parameter

---
+++ src/main/java/test/cicd/project/Utili/CreateEnvFile.java
@@ -0,0 +1,18 @@
+package test.cicd.project.Utili;
+
+import org.omg.CORBA.PUBLIC_MEMBER;
+
+import java.io.File;
+
+public class CreateEnvFile {
+
+    public void createFile() {
+
+
+        String envfile = "allure-results/environment.properties";
+        File createEnvFile = new File(envfile);
+
+
+
+    }
+}
