5e61a1138e64c037468016d8f85b0cf2456027c5
jun <hujy11@gmail.com>
2018-12-23 21:00:43 +0800

    testing link

--- src/test/java/test/cicd/project/runTest.java
+++ src/test/java/test/cicd/project/runTest.java
@@ -3,7 +3,6 @@ package test.cicd.project;
 import io.qameta.allure.*;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
-import org.testng.Assert;
 import org.testng.annotations.*;
 import org.testng.asserts.SoftAssert;
 import sun.misc.BASE64Decoder;
@@ -20,71 +19,74 @@ import static java.lang.String.format;
 import static org.testng.Assert.fail;
 
 @Epic("Regression Tests")
-@Feature("SDKPackage Testing")
+@Feature("Testing Practice")
 public class runTest {
 
     private WebDriver driver;
     private SetGetParameter setGetParameter;
 
     @BeforeTest
-    @Parameters({"runEnv","buildUrl","testUrl"})
-    public void setParameter(@Optional("testingstring") String runEnv , @Optional("testingBuildUrl") String buildUrl, @Optional("www.baidu.com") String testUrl) throws Exception {
+    @Parameters({"runEnv", "buildUrl", "testUrl"})
+    public void setParameter(@Optional("testingstring") String runEnv, @Optional("testingBuildUrl") String buildUrl, @Optional("www.baidu.com") String testUrl) throws Exception {
         setGetParameter = new SetGetParameter();
         setGetParameter.setLoginUser("svp_p_sdkuser");
         setGetParameter.setLoginPassword("OEI0Vll6emM=");
         setGetParameter.setBuildNo(buildUrl);
-        setGetParameter.getBuildNo();
+
         setGetParameter.setENV(runEnv);
-        setGetParameter.getEnv();
+
         setGetParameter.setBrowser(testUrl);
-        setGetParameter.getBrowser();
+
 
         System.out.println(decryptBase64(setGetParameter.getLoginPassword()));
         System.out.println(setGetParameter.getLoginUser());
     }
 
-    public String decryptBase64(String key) throws Exception{
+    public String decryptBase64(String key) throws Exception {
         byte result[] = (new BASE64Decoder()).decodeBuffer(key);
         return new String(result);
     }
+
     @AfterTest
-    public void createpropFile(){
+    public void createpropFile() {
         CreateEnvFile createprop = new CreateEnvFile();
-        createprop.createFile(setGetParameter.getEnv(),setGetParameter.getBuildNo(),setGetParameter.getBrowser());
+        createprop.createFile(setGetParameter.getEnv(), setGetParameter.getBuildNo(), setGetParameter.getBrowser());
     }
+
     @BeforeTest
     public void setupTest() {
         System.setProperty("webdriver.chrome.driver", "Driver/chromedriver.exe");
         driver = new ChromeDriver();
         //Create a new ChromeDriver
         //Go to www.swtestacademy.com
-        driver.navigate().to("http://www.google.com");
+        driver.navigate().to(setGetParameter.getBrowser());
     }
 
     //@Test(priority = 0, description = "SDKpackage verification")
     @Severity(SeverityLevel.CRITICAL)
     @Description("This cases run on Windows / MAC / Linux")
-    @Story("To verify SDKpackage")
+    @Story("To verify Url Title")
     public void testcase1() {
         SoftAssert softAssertion = new SoftAssert();
         VerifiedPackage verifiedPadk = new VerifiedPackage();
         verifiedPadk.createFoder();
-        if (!driver.getTitle().contains("google")){
-           softAssertion.fail("Testing no in google");
-           softAssertion.assertAll();
+        if (!driver.getTitle().contains("google")) {
+            softAssertion.fail("Testing not in google");
+            softAssertion.assertAll();
         }
         //Assert.assertTrue(driver.getTitle().contains("Software sevelopment"));
     }
 
-    @Test(priority = 1,description = "testcaseparam")
-    @Severity(SeverityLevel.NORMAL)
-    @Description("The case to scan the whole folder from aritifitaor")
-
-    public void testcaseparam() throws Exception {
+    @Test(priority = 1, description = "testParameter")
+    @Severity(SeverityLevel.BLOCKER)
+    @Description("To test Parameter")
+    @Link("http://www.google.com")
+    @Link(name = "allure",type = "mylink")
+    @Issue("123")
+    @TmsLink("test-1")
+    public void testParam() throws Exception {
         System.out.println(decryptBase64(setGetParameter.getLoginPassword()));
         System.out.println(setGetParameter.getLoginUser());
-
-
     }
 
     // @Test(priority = 1,description = "Binscope scan")
@@ -96,8 +98,8 @@ public class runTest {
 
     @Test(description = "CSV Attachment")
     public void csvAttachmentTest() throws Exception {
-        int i=0;
-        while (i<5) {
+        int i = 0;
+        while (i < 5) {
             saveCsvAttachment();
             i++;
         }
